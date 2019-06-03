d2893240c1a7d52992bc6d0789e3cffe7429fede
Michael Korir <mikor22@gmail.com>
2019-06-03 20:11:33 +0300

    another

--- manage.py
+++ manage.py
@@ -5,26 +5,30 @@ from flask_migrate import Migrate,MigrateCommand
 
 
 #Create app instance
-app = create_app('test')
-app=create_app("production")
+#app = create_app('test')
+# app=create_app("production")
 app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
 
-migrate = Migrate(app,db)
-manager.add_command('db',MigrateCommand)
 
 
-@manager.shell
-def make_shell_context():
-    return dict(app=app, db=db, User = User,Role = Role, Comment = Comment, Blog = Blog)
+
 
 @manager.command
 def test():
     import unittest
     tests=unittest.TestLoader().discover("tests")
     unittest.TextTestRunner(verbosity=2).run(tests)
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db, User = User,Role = Role, Comment = Comment, Blog = Blog)
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+
 
 if __name__ == '__main__':
     manager.run()
